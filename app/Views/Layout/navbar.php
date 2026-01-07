5db89f7db5074214e783c66ed48ccab10430680c
Jnakol <weejiriklho@icloud.com>
2026-01-07 08:02:09 +0100

    ahoj funguj

---
+++ app/Views/Layout/navbar.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-sm bg-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.html"><img src=""></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url(''); ?>">Nic speciálního</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('index.php/kardi'); ?>">tady stanice su</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</nav>
